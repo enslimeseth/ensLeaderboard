9ed100185983d938a23979123a9bb5ae30cf4303
enslimeseth <ens.limes.eth@gmail.com>
2024-01-11 21:11:04 -0500

    Updating metadatabase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'ENS Leaderboard',
   description: 'The most followed Farcaster accounts with .eth usernames',
+  metadataBase: new URL("https://ens-leaderboard.vercel.app"),
   openGraph: {images:["sharing.jpg"]}
 }
 
