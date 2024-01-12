2c5bf5319089bde9c424d7caeba65cecbe8828f9
dylsteck <dylancsteck@gmail.com>
2024-01-12 01:58:12 -0500

    Adds SWR, icons for previous/next, and mobile resizing/horizontal scroll for the table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,36 @@
-"use client";
-import axios from "axios";
-import Image from "next/image";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+"use client"
+import axios from 'axios';
+import { useState } from 'react';
+import useSWR from 'swr';
+import Link from 'next/link';
+import Image from 'next/image';
+import { ArrowLeftCircleIcon, ArrowRightCircleIcon } from '@heroicons/react/24/solid';
+
+const fetcher = (url: string) => axios.get(url).then(res => res.data);
+
+interface LeaderboardItem {
+  rank: string;
+  eth_name: string;
+  display: string;
+  pfp: string | null;
+  follower_count: string;
+}
 
 export default function Home() {
-  type LeaderboardItem = {
-    rank: string;
-    eth_name: string;
-    display: string;
-    pfp: string | null;
-    follower_count: string;
-  };
-
-  const [leaderboardData, setLeaderboardData] = useState<LeaderboardItem[] | null>(null);
-  const [cursor, setCursor] = useState<number>(0);
-
-  useEffect(() => {
-    async function getData(cursor: number) {
-      const localRoute = `https://api.farcasterkit.com/users/ensLeaderboard?cursor=${cursor}`;
-      const data = await axios.get(localRoute);
-      if (data) {
-        const final = data.data.leaderboard as LeaderboardItem[];
-        setLeaderboardData(final);
-      }
-    }
-    getData(cursor);
-  }, [cursor]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const { data, error } = useSWR(`https://api.farcasterkit.com/users/ensLeaderboard?cursor=${currentPage * 100}`, fetcher);
 
   const handleNext = () => {
-    setCursor((prevCursor) => prevCursor + 100);
+    setCurrentPage(prevPage => prevPage + 1);
   };
 
   const handlePrevious = () => {
-    setCursor((prevCursor) => (prevCursor > 0 ? prevCursor - 100 : 0));
+    setCurrentPage(prevPage => prevPage > 0 ? prevPage - 1 : 0);
   };
+
+  if (error) return <div>Failed to load</div>;
+
+  const leaderboardData: LeaderboardItem[] = data?.leaderboard || [];
 
   return (
     <div className="mt-10">
@@ -43,11 +39,11 @@ export default function Home() {
         <span className="text-[#432B8C]"> Farcaster</span>
       </h1>
       <h2 className="text-center">The most followed Farcaster accounts with .eth Farcaster names</h2>
-      <div className="flex justify-center mt-4">
-        <button onClick={handlePrevious} className="mr-2">Previous</button>
-        <button onClick={handleNext}>Next</button>
+      <div className="flex justify-center items-center mt-2 mb-4">
+        <ArrowLeftCircleIcon onClick={handlePrevious} className="mr-2 w-6 h-6" color="#806AED" />
+        <ArrowRightCircleIcon onClick={handleNext} className="mr-2 w-6 h-6" color="#806AED" />
       </div>
-      <div className="flex justify-center w-full overflow-x-auto">
+      <div className="overflow-x-auto">
         <table className="min-w-full">
           <thead>
             <tr className="bg-gray-200">
@@ -58,7 +54,7 @@ export default function Home() {
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-300">
-            {leaderboardData !== null && leaderboardData.map((item, index) => (
+            {leaderboardData.length > 0 ? leaderboardData.map((item, index) => (
               <tr key={index}>
                 <td className="px-4 py-2 text-left">{item.rank}</td>
                 <td className="px-4 py-2 text-left">
@@ -68,7 +64,7 @@ export default function Home() {
                 </td>
                 <td className="px-4 py-2 text-left">
                   <div className="flex flex-row items-center gap-2">
-                    <Image src={item.pfp || ""} alt={`PFP for ${item.eth_name} on Farcaster`} className="w-4 h-4 rounded-full" width={12} height={12} />
+                    <Image src={item.pfp || ""} alt={`PFP for ${item.eth_name} on Farcaster`} className="w-4 h-4 rounded-full" width={16} height={16} />
                     <Link className="underline decoration-[#432B8C]" href={`https://warpcast.com/${item.eth_name}`}>
                       {item.display}
                     </Link>
@@ -76,7 +72,7 @@ export default function Home() {
                 </td>
                 <td className="px-4 py-2 text-left">{Number(item.follower_count).toLocaleString()}</td>
               </tr>
-            ))}
+            )) : <tr><td colSpan={4}>Loading...</td></tr>}
           </tbody>
         </table>
       </div>
