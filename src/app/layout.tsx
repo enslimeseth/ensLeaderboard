20d073fff65c0c6cebedff1285981621ac98c045
dbrooo <ens.limes.eth@gmail.com>
2024-06-25 08:56:12 -0400

    Added Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Script from 'next/script'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -35,6 +36,7 @@ export default function RootLayout({
           });
         `}
       </Script>
+      <Analytics />
     </html>
   )
 }
